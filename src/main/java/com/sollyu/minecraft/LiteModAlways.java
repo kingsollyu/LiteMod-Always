fe3cdd00da333bcda6baaba923019d2141ef7c48
sollyu <sollyu@qq.com>
2017-09-16 22:11:41 +0800

    增加配置界面
      + 增加配置界面（在LiteLoader设置中）
      + 增加多语言支持
      * 更改状态提醒方式
      * 调整自动跳跃逻辑

--- src/main/java/com/sollyu/minecraft/LiteModAlways.java
+++ src/main/java/com/sollyu/minecraft/LiteModAlways.java
@@ -1,33 +1,38 @@
 package com.sollyu.minecraft;
 
+import com.google.gson.annotations.Expose;
 import com.mumfrey.liteloader.Configurable;
 import com.mumfrey.liteloader.Tickable;
 import com.mumfrey.liteloader.core.LiteLoader;
 import com.mumfrey.liteloader.modconfig.ConfigPanel;
-import com.mumfrey.liteloader.util.log.LiteLoaderLogger;
+import com.mumfrey.liteloader.modconfig.ConfigStrategy;
+import com.mumfrey.liteloader.modconfig.ExposableOptions;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.text.TextComponentString;
 import org.lwjgl.input.Keyboard;
 
 import java.io.File;
 
-
+@SuppressWarnings("WeakerAccess")
+@ExposableOptions(strategy = ConfigStrategy.Versioned, filename="always.json")
 public class LiteModAlways implements Tickable, Configurable {
 
     private static final KeyBinding swapKeyBinding = new KeyBinding("key.always.attack.toggle", Keyboard.KEY_F9, "key.categories.litemods");
 
     // 当前开关状态
-    private boolean attackEnabled = false;
+    private boolean enableAttack       = false;
+    private boolean enableJump         = false;
+    private boolean enableJumpByAttack = true;
 
     // 上一次执行操作的时间
     private Long attackLastExecuteTime = -1L;   // 上一次执行攻击的时间
     private Long attackLastJumpTime    = -1L;   // 上一次跳跃时间
 
-    // get
-    public boolean isAttackEnabled() {
-        return attackEnabled;
-    }
+    @Expose private Long alwaysJumpInterval   = 3 * 60 * 1000L; // 在没有攻击的情况下，多久跳跃一次
+    @Expose private Long alwaysAttackInterval = 1500L;          // 自动攻击间隔
 
     @Override
     public void init(File configPath) {
@@ -43,38 +48,42 @@ public class LiteModAlways implements Tickable, Configurable {
 
             // 控制键被按下
             if (LiteModAlways.swapKeyBinding.isPressed()) {
-                attackEnabled = !attackEnabled;
-                minecraft.player.sendChatMessage(String.format("/t %s always attack :%s", minecraft.player.getName(), attackEnabled ? "enable" : "disabled"));
+                enableAttack = !enableAttack;
+                enableJump = enableAttack;
+                minecraft.ingameGUI.getChatGUI().printChatMessage(new TextComponentString(I18n.format("always.message.attack.toggle", I18n.format(isEnableAttack() ? "always.string.enable" : "always.string.disabled"))));
             }
+
+            long currentTime = System.currentTimeMillis();
 
             // 使用时间差计算是否执行操作
             // 尽量减少计算，所以分了两个if
-            if (isAttackEnabled()) {
-                long currentTime = System.currentTimeMillis();
-                if (currentTime - attackLastExecuteTime > 1500 && minecraft.objectMouseOver.entityHit != null) {
+            if (isEnableAttack()) {
+                if (currentTime - attackLastExecuteTime > alwaysAttackInterval && minecraft.objectMouseOver.entityHit != null) {
                     minecraft.playerController.attackEntity(minecraft.player, minecraft.objectMouseOver.entityHit);
                     minecraft.player.swingArm(EnumHand.MAIN_HAND);
-                    // KeyBinding.onTick(minecraft.gameSettings.keyBindAttack.getKeyCode());
-                    attackLastExecuteTime = currentTime;
-                    attackLastJumpTime    = currentTime;
-                }
 
-                // 当5秒后没有对象攻击时就自己跳一下，防止AKF
-                if ((currentTime - attackLastJumpTime) > (3 * 60 * 1000)) {
-                    attackLastJumpTime    = currentTime;
-                    minecraft.player.jump();
+                    attackLastExecuteTime = currentTime;
+
+                    if (this.isEnableJumpByAttack()) {
+                        attackLastJumpTime = currentTime;
+                    }
                 }
             }
+
+            // 当5秒后没有对象攻击时就自己跳一下，防止AKF
+            if ((currentTime - attackLastJumpTime) > alwaysJumpInterval) {
+                attackLastJumpTime = currentTime;
+                minecraft.player.jump();
+            }
+
         } catch (Throwable e) {
             e.printStackTrace();
-            // TODO: java.lang.IllegalStateException: Can't overwrite cause with java.lang.NullPointerException
-            //LiteLoaderLogger.warning(e, e.getMessage());
         }
     }
 
     @Override
     public String getVersion() {
-        return "1.0.4";
+        return "1.0.5";
     }
 
     @Override
@@ -84,13 +93,52 @@ public class LiteModAlways implements Tickable, Configurable {
 
     @Override
     public String getName() {
-        return "Always";
+        return "always";
     }
-
 
     @Override
     public Class<? extends ConfigPanel> getConfigPanelClass() {
         return LiteModAlwaysConfigPanel.class;
     }
 
+    // get & set
+    public boolean isEnableAttack() {
+        return enableAttack;
+    }
+
+    public boolean isEnableJump() {
+        return enableJump;
+    }
+
+    public void setEnableJump(boolean enableJump) {
+        this.enableJump = enableJump;
+    }
+
+    public boolean isEnableJumpByAttack() {
+        return enableJumpByAttack;
+    }
+
+    public void setEnableJumpByAttack(boolean enableJumpByAttack) {
+        this.enableJumpByAttack = enableJumpByAttack;
+    }
+
+    public void setEnableAttack(boolean enableAttack) {
+        this.enableAttack = enableAttack;
+    }
+
+    public Long getAlwaysJumpInterval() {
+        return alwaysJumpInterval;
+    }
+
+    public void setAlwaysJumpInterval(Long alwaysJumpInterval) {
+        this.alwaysJumpInterval = alwaysJumpInterval;
+    }
+
+    public Long getAlwaysAttackInterval() {
+        return alwaysAttackInterval;
+    }
+
+    public void setAlwaysAttackInterval(Long alwaysAttackInterval) {
+        this.alwaysAttackInterval = alwaysAttackInterval;
+    }
 }
--- src/main/java/com/sollyu/minecraft/LiteModAlwaysConfigPanel.java
+++ src/main/java/com/sollyu/minecraft/LiteModAlwaysConfigPanel.java
@@ -1,6 +1,7 @@
 package com.sollyu.minecraft;
 
 import com.mumfrey.liteloader.client.gui.GuiCheckbox;
+import com.mumfrey.liteloader.core.LiteLoader;
 import com.mumfrey.liteloader.modconfig.AbstractConfigPanel;
 import com.mumfrey.liteloader.modconfig.ConfigPanelHost;
 import net.minecraft.client.resources.I18n;
@@ -9,13 +10,40 @@ import net.minecraft.client.resources.I18n;
  * 配置界面
  */
 public class LiteModAlwaysConfigPanel extends AbstractConfigPanel {
+
+    public static final int SPACING     = 8;
+    public static final int ITEM_HEIGHT = 20;
+
+    private LiteModAlways liteModAlways = null;
+
+    // 自动攻击GUI
+    private GuiCheckbox     attackGuiCheckbox     = null;
+    private ConfigTextField attackConfigTextField = null;
+
+    private GuiCheckbox     jumpGuiCheckbox         = null;
+    private GuiCheckbox     jumpByAttackGuiCheckbox = null;
+    private ConfigTextField jumpConfigTextField     = null;
+
     @Override
     protected void addOptions(ConfigPanelHost host) {
+        liteModAlways = host.getMod();
 
-        this.addLabel(1, 0, 0, 200, 32, 0xFFFF55, "攻击间隔");
+        int id = 0;
 
         try {
-            this.addTextField(2, 0, 200, 200, 32 ).setText("900");
+            attackGuiCheckbox     = this.addControl(new GuiCheckbox(id++, 20, (id - 1) * SPACING + 5, I18n.format("always.panel.attack.check")), checkboxConfigOptionListener);
+            attackConfigTextField = this.addTextField(id++, attackGuiCheckbox.getButtonWidth() + 25, attackGuiCheckbox.yPosition, 40, 12 /* LiteLoader的CheckBox默认高度为12 */);
+
+            jumpGuiCheckbox         = this.addControl(new GuiCheckbox(id++, 20, (id - 1) * SPACING + 5, I18n.format("always.panel.jump.check")), checkboxConfigOptionListener);
+            jumpByAttackGuiCheckbox = this.addControl(new GuiCheckbox(id++, jumpGuiCheckbox.getButtonWidth() + 20, jumpGuiCheckbox.yPosition, I18n.format("always.panel.jump.attack.check")), checkboxConfigOptionListener);
+            jumpConfigTextField     = this.addTextField(id++, jumpByAttackGuiCheckbox.xPosition + jumpByAttackGuiCheckbox.getButtonWidth() + 5, jumpByAttackGuiCheckbox.yPosition, 50, 12 /* LiteLoader的CheckBox默认高度为12 */);
+
+            attackGuiCheckbox.checked = getLiteModAlways().isEnableAttack();
+            attackConfigTextField.setText(String.valueOf(getLiteModAlways().getAlwaysAttackInterval()));
+
+            jumpGuiCheckbox.checked = getLiteModAlways().isEnableJump();
+            jumpByAttackGuiCheckbox.checked = getLiteModAlways().isEnableJumpByAttack();
+            jumpConfigTextField.setText(String.valueOf(getLiteModAlways().getAlwaysJumpInterval()));
         } catch (Throwable e) {
             e.printStackTrace();
         }
@@ -23,11 +51,45 @@ public class LiteModAlwaysConfigPanel extends AbstractConfigPanel {
 
     @Override
     public String getPanelTitle() {
-        return "Hi ==============" + I18n.format("app.name");
+        return I18n.format("always.app.name");
     }
 
     @Override
     public void onPanelHidden() {
+        getLiteModAlways().setAlwaysAttackInterval(Long.valueOf(attackConfigTextField.getText()));
+        getLiteModAlways().setEnableAttack(attackGuiCheckbox.checked);
 
+        getLiteModAlways().setEnableJump(jumpGuiCheckbox.checked);
+        getLiteModAlways().setEnableJumpByAttack(jumpByAttackGuiCheckbox.checked);
+        getLiteModAlways().setAlwaysJumpInterval(Long.valueOf(jumpConfigTextField.getText()));
+
+        LiteLoader.getInstance().writeConfig(getLiteModAlways());
+    }
+
+    private ConfigOptionListener<GuiCheckbox> checkboxConfigOptionListener = new ConfigOptionListener<GuiCheckbox>() {
+        @Override
+        public void actionPerformed(GuiCheckbox control) {
+            control.checked = !control.checked;
+        }
+    };
+
+
+//    /**
+//     * 自动攻击间隔，
+//     */
+//    private ConfigOptionListener<GuiButton> attackGuiButtonConfigOptionListener = new ConfigOptionListener<GuiButton>() {
+//        @Override
+//        public void actionPerformed(GuiButton control) {
+//            int attackIntervalCurrentPosition = attackIntervalDefaultList.indexOf(getLiteModAlways().getAlwaysAttackInterval()) + 1;
+//            if (attackIntervalCurrentPosition >= attackIntervalDefaultList.size()) {
+//                attackIntervalCurrentPosition = 0;
+//            }
+//            getLiteModAlways().setAlwaysAttackInterval(attackIntervalDefaultList.get(attackIntervalCurrentPosition));
+//            control.displayString = "时间间隔：" + getLiteModAlways().getAlwaysAttackInterval();
+//        }
+//    };
+
+    private LiteModAlways getLiteModAlways() {
+        return liteModAlways;
     }
 }
